cb05a39b88021819048b09ccf0ab0c1aa9dfdca0
rahathossain3 <rahathossain210@gmail.com>
2024-05-17 16:55:48 +0600

    new chapter

---
+++ 2. Advance Javascript/scope.js
@@ -0,0 +1,26 @@
+// block
+// global
+// functional
+
+/* const name = 'Alex';
+
+function show() {
+    console.log(name)
+}
+
+show() */
+
+/* 
+function show() {
+    const name = 'alex';
+    console.log(name)
+}
+
+console.log(name)
+show()
+*/
+
+{
+    let name = 'john';
+    console.log(name)
+}
